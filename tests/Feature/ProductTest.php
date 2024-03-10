6847b7b9c2bd89372cbda413974c2e75ae1dd4c6
gofran04 <docreader04@gmail.com>
2024-03-10 17:37:42 +0200

    test_an_auth_user_can_read_specific_product

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -24,5 +24,26 @@ class ProductTest extends TestCase
         $this->assertDatabaseCount('products', 8);//3 this will pass
 
     }
+
+    public function test_an_auth_user_can_read_a_product()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $product = Product::factory()->create();
+
+        $response = $this->get('/api/products/'.$product->id);
+
+        $response->assertSuccessful();
+        $response->assertJson([
+               'data' => [
+                   'id'          => $product->id,
+                   'name'        => $product->name,
+                   'description' => $product->description,
+                   'price'       => $product->price,
+                   'category_id' => $product->category_id,
+               ]
+           ]);
+
+    }
     
 }
