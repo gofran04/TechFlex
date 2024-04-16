ee5ee6d51bda2494dd17077b09b46878395e99e5
gofran04 <docreader04@gmail.com>
2024-04-16 17:51:47 +0200

    add pictue for each category

--- app/Http/Requests/Category/UpdateCategoryRequest.php
+++ app/Http/Requests/Category/UpdateCategoryRequest.php
@@ -23,6 +23,7 @@ class UpdateCategoryRequest extends FormRequest
     {
         return [
             'name'              => ['required', 'string'],
+            'category_pic'        => ['file'],
         ];
     }
 }
