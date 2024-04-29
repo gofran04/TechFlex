d796f6505d925fe0fa0aed9f49452bf822b93df8
gofran04 <docreader04@gmail.com>
2024-04-29 23:03:00 +0200

    only authorized users can view all users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
 
     public function index()
     {
+        $this->authorize('view-all-users');
         $users = User::all();
         return UserResource::collection($users);
     }
