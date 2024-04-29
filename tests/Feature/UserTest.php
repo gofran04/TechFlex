9e686c4a9dc35dff2447e743713017ffa66b2566
gofran04 <docreader04@gmail.com>
2024-04-29 23:16:22 +0200

    test_an_authenticated_and_authorized_user_can_read_all_users

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Database\Seeders\GeneralManagerSeeder;
+use Database\Seeders\PermissionsSeeder;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed([
+        GeneralManagerSeeder::class,
+        PermissionsSeeder::class,
+        ]);
+    }
+
+    public function test_an_authenticated_and_authorized_user_can_read_all_users()
+    {
+        $user = User::factory()->create();
+        $user->assignRole('supervisor');
+        $this->actingAs($user);
+
+        User::factory()->count(8)->create();
+
+        $response = $this->get('/api/users');
+        $response->assertSuccessful();
+        $this->assertDatabaseCount('users', count($response['data']));
+    }
+
+}
+
