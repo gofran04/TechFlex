a50d35e7f7b7a782458184954ddbb64bf04c617b
gofran04 <docreader04@gmail.com>
2024-05-03 13:58:28 +0200

    test_an_authenticated_and_authorized_user_can_update_a_user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,13 +25,14 @@ class UserFactory extends Factory
     {
         $num = random_int(100000000, 999999999);
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'address'   => fake()->address(),
-            'phone'     => '0'.$num,
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
+            'address'           => fake()->address(),
+            'phone'             => '0'.$num,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
+            'type'              => 'driver',
         ];
     }
 
