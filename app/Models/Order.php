49cd2842394f3bfb591324bc448c952101a24036
gofran04 <docreader04@gmail.com>
2024-05-13 23:35:32 +0200

    made model (orderProduct) plus migration

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
 
 class Order extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'client_id',
@@ -19,6 +17,11 @@ class Order extends Model
     public function client()
     {
       return $this->belongsTo(User::class,'client_id');
+    }
+
+    public function orderProducts()
+    {
+      return $this->hasMany(OrderProduct::class);
     }
 
 }
