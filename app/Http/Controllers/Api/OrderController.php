c8924b369af9ca490743e504054700234ac00471
gofran04 <docreader04@gmail.com>
2024-05-20 17:09:50 +0200

    update order(order's status)

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,10 +8,11 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Order\StoreOrderRequest;
+use App\Http\Requests\Order\UpdateOrderRequest;
 use App\Http\Resources\OrderResource;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
-
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
@@ -87,11 +88,29 @@ class OrderController extends Controller
         return new OrderResource($order);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Order $order)
+    public function update(UpdateOrderRequest $request, Order $order)
     {
-        //
+        $this->authorize('edit-order');
+        $order->update($request->validate());
+
+        switch ($request->status) {
+            case 'in process':
+                $order->update(['status' => 'in process', 'driver_id' => $request->driver_id]);
+                break;            
+            case 'out for delivery':
+                $order->update(['status' => 'out for delivery', 'taken_at' => Carbon::now()]);
+                break;
+            case 'delivered':
+                $order->update(['status' => 'delivered','delivered_at' => Carbon::now()]);
+            break;
+            case 'canceled':
+                $order->update(['status' => 'canceled']);
+            break;
+        }
+
+        return (new OrderResource($order))
+                    ->response()
+                    ->setStatusCode(Response::HTTP_CREATED);
+        
     }
 }
