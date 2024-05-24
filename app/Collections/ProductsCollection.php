7864ddf3690e141af3bc43dddc32bb66b3e51c21
gofran04 <docreader04@gmail.com>
2024-05-24 15:51:15 +0200

    filter productss

---
+++ app/Collections/ProductsCollection.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\Collections;
+
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
+
+
+class ProductsCollection
+{
+    public static function collection(Request $request)
+    {
+        $defaultSort = '-created_at';
+
+        $defaultSelect = [
+            'id',
+            'name',
+            'category_id',
+            'price',
+            'description',
+            'created_at',
+            'updated_at',
+        ];
+
+        $allowedFilters = [
+            AllowedFilter::exact('id'),
+            'name',
+            'category_id',
+            'price',
+            'description',
+            'created_at',
+            'updated_at',
+        ];
+
+        $allowedSorts = [
+            'price',
+            'updated_at',
+            'created_at',
+        ];
+
+        $perPage = $request->limit  ? $request->limit : 50;
+
+        return QueryBuilder::for(Product::class)
+            ->select($defaultSelect)
+            ->allowedFilters($allowedFilters)
+            ->allowedSorts($allowedSorts)
+            ->defaultSort($defaultSort)
+            ->paginate($perPage);
+    }
+
+}
