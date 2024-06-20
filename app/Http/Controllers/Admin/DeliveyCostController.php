bb7c1a18fb4d28bf7bc9426cef35536881d4e846
gofran04 <docreader04@gmail.com>
2024-06-20 12:03:52 +0200

    Creat (Delivery Cost) Model with migration,seeder and factory

---
+++ app/Http/Controllers/Admin/DeliveyCostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Models\DeliveyCost;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DeliveyCostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(DeliveyCost $deliveyCost)
+    {
+        //
+    }
+
+    public function update(Request $request, DeliveyCost $deliveyCost)
+    {
+        //
+    }
+
+    public function destroy(DeliveyCost $deliveyCost)
+    {
+        //
+    }
+}
---
+++ database/factories/DeliveyCostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DeliveyCost>
+ */
+class DeliveyCostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'area'           => fake()->address(),
+            'delivey_cost'   => 1200,
+        ];
+    }
+}
