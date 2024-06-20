4879aaf502f58e77ab6b85e7109cdf57570241c2
gofran04 <docreader04@gmail.com>
2024-06-20 12:41:49 +0200

    fix model name from deliveyCost To deliverycost

---
+++ database/seeders/DeliveryCostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DeliveryCost;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DeliveryCostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DeliveryCost::factory()->create(['area' => 'Area 1', 'delivery_cost' => 1000.00]);
+        DeliveryCost::factory()->create(['area' => 'Area 1', 'delivery_cost' => 2000.00]);
+        DeliveryCost::factory()->create(['area' => 'Area 1', 'delivery_cost' => 3000.00]);
+        // DeliveryCost::factory()->count(1)->create();
+    }
+}
