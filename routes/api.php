bd61a1c3c41e5a2ef81d29fd34fd9187ea5bf7e3
gofran04 <docreader04@gmail.com>
2024-07-10 23:52:15 +0200

    only authenticated and authorized user can update Store model

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'guest'], function (){
     Route::post('login',[AuthController::class, 'login'])->name('login');
     Route::apiResource('products',ProductController::class)->only(['index','show']);
     Route::apiResource('categories',CategoryController::class)->only(['index','show']);
-    Route::apiResource('stores',StoreController::class)->except(['store','destroy']);;
+    Route::apiResource('stores',StoreController::class)->only(['index','show']);
 
 });
 
@@ -35,4 +35,5 @@ Route::group(['middleware' => 'auth'],function(){
     Route::apiResource('categories',CategoryController::class)->only(['store','update','destroy']);
     Route::apiResource('orders',OrderController::class)->except(['destroy']);
     Route::apiResource('users',UserController::class);
+    Route::apiResource('stores',StoreController::class)->only(['update']);
 });
