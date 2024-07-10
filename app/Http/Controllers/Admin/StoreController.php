7d084d390d409befc6833e82fc1354a00f2fef92
gofran04 <docreader04@gmail.com>
2024-07-10 23:52:15 +0200

    only authenticated and authorized user can update Store model

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Store;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\StoreResource;
+use App\Http\Requests\Store\UpdateStoreRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class StoreController extends Controller
 {
@@ -19,8 +20,13 @@ class StoreController extends Controller
         return new StoreResource($store);
     }
 
-    public function update(Request $request, Store $store)
+    public function update(UpdateStoreRequest $request, Store $store)
     {
-        //
+        $this->authorize('edit-store');
+        $store->update($request->validated());
+
+        return (new StoreResource($store->refresh()))
+            ->response()
+            ->setStatusCode(Response::HTTP_ACCEPTED);
     }
 }
--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -46,7 +46,9 @@ class PermissionsSeeder extends Seeder
             'view-all-orders',
             'view-order',
             'create-order',
-            'edit-order',        
+            'edit-order', 
+            
+            'edit-store',
         ];
 
         foreach ($permissions as $permission) 
