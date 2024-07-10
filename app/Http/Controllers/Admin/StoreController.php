8f1faaf2a7a9b3fc13cda6c5a5e70790c37489e8
gofran04 <docreader04@gmail.com>
2024-07-10 23:30:02 +0200

    Create Store Model with migration, controller,factory and seeder.There is no store function on controller. store data will inserted to db by seeder

---
+++ app/Http/Controllers/Admin/StoreController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Store;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StoreController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function show(Store $store)
+    {
+        //
+    }
+
+    public function update(Request $request, Store $store)
+    {
+        //
+    }
+
+    public function destroy(Store $store)
+    {
+        //
+    }
+}
---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Store;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Store::factory()->create([
+            'name'              => "TechFlex",
+            'email'             => 'Techflex@mail.com',
+            'address'           => 'TechFlex Address- street 017',
+            'phone'             => '0123456789',
+        ]);
+    }
+}
