3c37e76dfd3d72d6790248a7861dc0730e453725
gofran04 <docreader04@gmail.com>
2024-07-10 23:30:02 +0200

    Create Store Model with migration, controller,factory and seeder.There is no store function on controller. store data will inserted to db by seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            StoreSeeder::class,
             GeneralManagerSeeder::class,
             PermissionsSeeder::class,
             UserSeeder::class,
